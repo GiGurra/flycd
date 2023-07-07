f0224ec18e40da1bdd4cfe29b9a86d2c8c251b33
Johan Kjölhede <johan.kjolhede@ingrid.com>
2023-07-07 20:14:00 +0200

    wip

--- example-projects/example-project1/app1/test-launch.bash
+++ example-projects/example-project1/app1/test-launch.bash
@@ -4,7 +4,7 @@ set -e
 
 cat app.yaml | yq | jq '."fly.toml".overwrite' | yj -jt > fly.toml
 
-fly launch --ha=false --auto-confirm --now --copy-config --name=project1-app1-foobar12341
+fly launch --ha=false --auto-confirm --now --copy-config --name=example-project1-app1-foobar12341
 
 
 
