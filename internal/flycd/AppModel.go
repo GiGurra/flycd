7abb0331f5f0c1e04a74ad3cf04a7d0c78eff1f5
Johan Kjölhede <johan.kjolhede@ingrid.com>
2023-07-07 21:46:54 +0200

    wip

---
+++ internal/flycd/AppModel.go
@@ -0,0 +1,90 @@
+package flycd
+
+import (
+	"fmt"
+	"regexp"
+)
+
+type AppConfig struct {
+	App          string            `yaml:"app"`
+	Source       Source            `yaml:"source"`
+	Services     []Service         `yaml:"services"`
+	LaunchParams []string          `yaml:"launch_params"`
+	DeployParams []string          `yaml:"deploy_params"`
+	Env          map[string]string `yaml:"env"`
+}
+
+func (a *AppConfig) Validate() error {
+	if a.App == "" {
+		return fmt.Errorf("app name is required")
+	}
+
+	// only permit apps that are valid dns names
+	const subdomainPrefixRegExp = `^[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?$`
+
+	subdomainPrefixChecker := regexp.MustCompile(subdomainPrefixRegExp)
+	if !subdomainPrefixChecker.MatchString(a.App) {
+		return fmt.Errorf("app name '%s' is not a valid subdomain prefix", a.App)
+	}
+
+	err := a.Source.Validate()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type Source struct {
+	Repo string `yaml:"repo"`
+	Path string `yaml:"path"`
+	Ref  string `yaml:"ref"`
+	Type string `yaml:"type"`
+}
+
+func (s *Source) Validate() error {
+
+	if s.Repo == "" {
+		return fmt.Errorf("repo is required")
+	}
+
+	switch s.Type {
+	case "git":
+	case "local":
+	default:
+		return fmt.Errorf("invalid source type: %s", s.Type)
+	}
+
+	if s.Path == "" {
+		return fmt.Errorf("path is required")
+	}
+
+	if s.Type == "git" && s.Ref == "" {
+		return fmt.Errorf("ref is required for git source type")
+	}
+
+	return nil
+}
+
+type Concurrency struct {
+	Type      string `yaml:"type"`
+	SoftLimit int    `yaml:"soft_limit"`
+	HardLimit int    `yaml:"hard_limit"`
+}
+
+type Ports struct {
+	Handlers   []string `yaml:"handlers"`
+	Port       int      `yaml:"port"`
+	ForceHttps bool     `yaml:"force_https"`
+}
+
+type Service struct {
+	InternalPort       int         `yaml:"internal_port"`
+	Protocol           string      `yaml:"protocol"`
+	ForceHttps         bool        `yaml:"force_https"`
+	AutoStopMachines   bool        `yaml:"auto_stop_machines"`
+	AutoStartMachines  bool        `yaml:"auto_start_machines"`
+	MinMachinesRunning int         `yaml:"min_machines_running"`
+	Concurrency        Concurrency `yaml:"concurrency"`
+	Ports              []Ports     `yaml:"ports"`
+}
---
+++ internal/flycd/flycd.go
@@ -0,0 +1,198 @@
+package flycd
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v3"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func isSymLink(dir os.DirEntry) (bool, error) {
+	info, err := dir.Info()
+	if err != nil {
+		return false, fmt.Errorf("error getting symlink info: %w", err)
+	}
+
+	return info.Mode().Type() == os.ModeSymlink, nil
+}
+
+func canTraverseDir(dir os.DirEntry) (bool, error) {
+
+	symlink, err := isSymLink(dir)
+	if err != nil {
+		return false, err
+	}
+
+	if symlink {
+		return false, nil
+	}
+
+	if dir.Name() == ".git" || dir.Name() == ".actions" || dir.Name() == ".idea" || dir.Name() == ".vscode" {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+func Deploy(path string) error {
+	// Traverse all child dirs and deploy them, as long as they are not symlinks
+
+	println("Deploying:", path)
+
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		return fmt.Errorf("error reading directory: %w", err)
+	}
+
+	traversableCandidates, hasAppYaml, hasProjectsDir, err := analyseFolder(entries)
+	if err != nil {
+		return fmt.Errorf("error analysing folder: %w", err)
+	}
+
+	if hasAppYaml {
+		fmt.Printf("Found app.yaml in %s, deploying app\n", path)
+
+		// create a temp tempDir
+		tempDir, err := os.MkdirTemp("", "flycd")
+		if err != nil {
+			return fmt.Errorf("error creating temp tempDir: %w", err)
+		}
+		defer func(dir string) {
+			err := os.RemoveAll(dir)
+			if err != nil {
+				println("error removing temp tempDir:", err)
+			}
+		}(tempDir)
+
+		// read the original app.yaml file using a go yaml library
+		var cfg AppConfig
+
+		appYaml, err := os.ReadFile(path + "/app.yaml")
+		if err != nil {
+			return fmt.Errorf("error reading app.yaml from folder %s: %w", path, err)
+		}
+
+		err = yaml.Unmarshal(appYaml, &cfg)
+		if err != nil {
+			return fmt.Errorf("error unmarshalling app.yaml from folder %s: %w", path, err)
+		}
+
+		fmt.Printf("app.yaml:\n%+v\n", cfg)
+
+		// Validate the app config
+		err = cfg.Validate()
+		if err != nil {
+			return fmt.Errorf("error validating app.yaml from folder %s: %w", path, err)
+		}
+
+		// Clone the git repo
+		switch cfg.Source.Type {
+		case "git":
+			_, err = runCommand(tempDir, "git", "clone", cfg.Source.Repo, "repo")
+			if err != nil {
+				return fmt.Errorf("error cloning git repo %s: %w", cfg.Source.Repo, err)
+			}
+			tempDir = tempDir + "/repo"
+		case "local":
+			// Copy the local folder to the temp tempDir
+			_, err = runCommand(tempDir, "sh", "-c", fmt.Sprintf("cp -r \"%s\" \"%s\"", cfg.Source.Path, tempDir))
+			if err != nil {
+				return fmt.Errorf("error copying local folder %s: %w", cfg.Source.Path, err)
+			}
+		default:
+			return fmt.Errorf("unknown source type %s", cfg.Source.Type)
+		}
+
+		// Produce the fly.toml file
+		_, err = runCommand(tempDir, "sh", "-c", fmt.Sprintf("cat \"%s/fly.toml\"", tempDir))
+		if err != nil {
+			return fmt.Errorf("error reading fly.toml from folder %s: %w", path, err)
+		}
+
+		// execute 'cat app.yaml | yj -yt > fly.toml' on the command line
+		cmd := exec.Command("sh", "-c", fmt.Sprintf("cat \"%s/app.yaml\" | yj -yt > \"%s/fly.toml\"", path, tempDir))
+		_, err = cmd.Output()
+
+		if err != nil {
+			return fmt.Errorf("error producing fly.toml from app.yaml in folder %s: %w", path, err)
+		}
+
+		// Now run flyctl and check if the app exists
+		_, err = runCommand(tempDir, "flyctl", "status")
+		if err != nil {
+			if strings.Contains(strings.ToLower(err.Error()), "could not find app") {
+				println("App not found, creating it")
+			} else {
+				return fmt.Errorf("error running flyctl status in folder %s: %w", path, err)
+			}
+		}
+
+	} else if hasProjectsDir {
+		println("Found projects dir, traversing only projects")
+		return Deploy(path + "/projects")
+	} else {
+		println("Found neither app.yaml nor projects dir, traversing all dirs")
+		for _, entry := range traversableCandidates {
+			err := Deploy(path + "/" + entry.Name())
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func analyseFolder(entries []os.DirEntry) ([]os.DirEntry, bool, bool, error) {
+	// Collect potentially traversable dirs
+	traversableCandidates := make([]os.DirEntry, 0)
+	hasAppYaml := false
+	hasProjectsDir := false
+
+	for _, entry := range entries {
+		if entry.IsDir() {
+			shouldTraverse, err := canTraverseDir(entry)
+			if err != nil {
+				return nil, false, false, fmt.Errorf("error checking for symlink: %w", err)
+			}
+			if !shouldTraverse {
+				continue
+			}
+
+			if entry.Name() == "projects" {
+				hasProjectsDir = true
+			}
+
+			traversableCandidates = append(traversableCandidates, entry)
+
+		} else if entry.Name() == "app.yaml" {
+
+			hasAppYaml = true
+
+		}
+	}
+	return traversableCandidates, hasAppYaml, hasProjectsDir, nil
+}
+
+type Command struct {
+	App  string
+	Args []string
+	Cwd  string
+}
+
+func runCommand(cwd string, command string, args ...string) (string, error) {
+	cmd := exec.Command(command, args...)
+	cmd.Dir = cwd
+	out, err := cmd.Output()
+	if err != nil {
+
+		stdErr := ""
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			stdErr = string(exitErr.Stderr)
+		}
+		return string(out), fmt.Errorf("error running command %s \n %s: %w", command, stdErr, err)
+	}
+
+	return string(out), nil
+}
