b6374f0da8dee2539d1c4c220c102c1706b23d6c
Johan Kjölhede <johan.kjolhede@ingrid.com>
2023-07-08 18:57:17 +0200

    wip

--- main.go
+++ main.go
@@ -10,32 +10,33 @@ import (
 
 const Version = "v0.0.5"
 
+var deployCmd = &cobra.Command{
+	Use:   "deploy",
+	Short: "Manually deploy a single flycd app, or all flycd apps inside a folder",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("deploy called")
+		path := args[0]
+		fmt.Printf("Deploying from: %s\n", path)
+		err := flycd.Deploy(path)
+		if err != nil {
+			fmt.Printf("Error deploying from %s: %v\n:", path, err)
+			os.Exit(1)
+		}
+	},
+}
+
 var rootCmd = &cobra.Command{
 	Use:   "flycd",
 	Short: "flycd deployment of fly apps entirely from code, without manual flyctl commands... I hope :D",
 	Long:  `Complete documentation is available at https://github.com/gigurra/flycd`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// Do Stuff Here
-		fmt.Printf("Hello World, args: %+v\n", args)
-		if len(args) == 1 {
-			fmt.Printf("Deploying from: %s\n", args[0])
-			err := flycd.Deploy(args[0])
+		if len(args) < 1 {
+			err := cmd.Help() // Display help message
 			if err != nil {
-				fmt.Println("Error deploying:", err)
-				os.Exit(1)
+				fmt.Printf("Error displaying help: %v\n", err)
 			}
-		} else if len(args) == 0 {
-			// use current workdir
-			cwd := os.Getenv("PWD")
-			fmt.Printf("Using current workdir: %s\n", cwd)
-			err := flycd.Deploy(cwd)
-			if err != nil {
-				fmt.Println("Error deploying:", err)
-				os.Exit(1)
-			}
-		} else {
-			fmt.Printf("Usage: flycd [path]\n")
-			os.Exit(1)
+			os.Exit(1) // Exit with code 1
 		}
 	},
 }
@@ -53,13 +54,14 @@ func main() {
 		}
 	}
 
+	// prepare cli
+	rootCmd.AddCommand(deployCmd)
+
+	// run cli
 	if err := rootCmd.Execute(); err != nil {
 		_, _ = fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
-	// print all cli arguments
-	for _, arg := range os.Args[1:] {
-		print(arg)
-	}
+	fmt.Printf("FlyCD %s exiting normally, bye!\n", Version)
 }
