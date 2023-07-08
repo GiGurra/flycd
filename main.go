2287e919098bac45f8a1b9a1284e2f710c7ea664
Johan Kjölhede <johan.kjolhede@ingrid.com>
2023-07-08 19:30:17 +0200

    wip

--- main.go
+++ main.go
@@ -10,22 +10,6 @@ import (
 
 const Version = "v0.0.5"
 
-var deployCmd = &cobra.Command{
-	Use:   "deploy",
-	Short: "Manually deploy a single flycd app, or all flycd apps inside a folder",
-	Args:  cobra.ExactArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("deploy called")
-		path := args[0]
-		fmt.Printf("Deploying from: %s\n", path)
-		err := flycd.Deploy(path)
-		if err != nil {
-			fmt.Printf("Error deploying from %s: %v\n:", path, err)
-			os.Exit(1)
-		}
-	},
-}
-
 var rootCmd = &cobra.Command{
 	Use:   "flycd",
 	Short: "flycd deployment of fly apps entirely from code, without manual flyctl commands... I hope :D",
@@ -36,8 +20,112 @@ var rootCmd = &cobra.Command{
 			if err != nil {
 				fmt.Printf("Error displaying help: %v\n", err)
 			}
+			err = cmd.Usage()
+			if err != nil {
+				fmt.Printf("error displaying usage: %v\n", err)
+			}
 			os.Exit(1) // Exit with code 1
 		}
+	},
+}
+
+var deployCmd = &cobra.Command{
+	Use:   "deploy",
+	Short: "Manually deploy a single flycd app, or all flycd apps inside a folder",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		path := args[0]
+		fmt.Printf("Deploying from: %s\n", path)
+		err := flycd.Deploy(path)
+		if err != nil {
+			fmt.Printf("Error deploying from %s: %v\n:", path, err)
+			os.Exit(1)
+		}
+	},
+}
+
+var monitorCmd = &cobra.Command{
+	Use:   "monitor",
+	Short: "Monitor a single flycd app, or all flycd apps inside a folder (recursively)",
+	Args:  cobra.RangeArgs(0, 1),
+	Run: func(cmd *cobra.Command, args []string) {
+		path, err := os.Getwd()
+		if err != nil {
+			fmt.Printf("Error getting current working directory: %v\n", err)
+			os.Exit(1)
+		}
+
+		if len(args) > 0 {
+			path = args[0]
+		}
+
+		fmt.Printf("Monitoring: %s\n", path)
+
+		fmt.Printf("Not implemented yet, sorry :(\n")
+		os.Exit(1)
+	},
+}
+
+var installCmd = &cobra.Command{
+	Use:   "install",
+	Short: "Install flycd into your fly.io account, listening to webhooks from this cfg repo and your app repos",
+	Run: func(cmd *cobra.Command, args []string) {
+		path, err := os.Getwd()
+		if err != nil {
+			fmt.Printf("Error getting current working directory: %v\n", err)
+			os.Exit(1)
+		}
+
+		if len(args) > 0 {
+			path = args[0]
+		}
+
+		fmt.Printf("Monitoring: %s\n", path)
+
+		fmt.Printf("Not implemented yet, sorry :(\n")
+		os.Exit(1)
+	},
+}
+
+var upgradeCmd = &cobra.Command{
+	Use:   "upgrade",
+	Short: "Upgrade your flycd installation in your fly.io account to the latest version",
+	Run: func(cmd *cobra.Command, args []string) {
+		path, err := os.Getwd()
+		if err != nil {
+			fmt.Printf("Error getting current working directory: %v\n", err)
+			os.Exit(1)
+		}
+
+		if len(args) > 0 {
+			path = args[0]
+		}
+
+		fmt.Printf("Monitoring: %s\n", path)
+
+		fmt.Printf("Not implemented yet, sorry :(\n")
+		os.Exit(1)
+	},
+}
+
+var uninstallCmd = &cobra.Command{
+	Use:   "uninstall",
+	Short: "Uninstall flycd from your fly.io account",
+	Run: func(cmd *cobra.Command, args []string) {
+		path, err := os.Getwd()
+		if err != nil {
+			fmt.Printf("Error getting current working directory: %v\n", err)
+			os.Exit(1)
+		}
+
+		if len(args) > 0 {
+			path = args[0]
+		}
+
+		fmt.Printf("Monitoring: %s\n", path)
+
+		fmt.Printf("Not implemented yet, sorry :(\n")
+		os.Exit(1)
 	},
 }
 
@@ -55,7 +143,7 @@ func main() {
 	}
 
 	// prepare cli
-	rootCmd.AddCommand(deployCmd)
+	rootCmd.AddCommand(deployCmd, monitorCmd, installCmd, upgradeCmd, uninstallCmd)
 
 	// run cli
 	if err := rootCmd.Execute(); err != nil {
