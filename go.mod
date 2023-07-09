8665edf1813b66f15874e88324b4a240ef50dd1d
Johan Kjölhede <johan.kjolhede@ingrid.com>
2023-07-09 13:31:03 +0200

    wip

--- go.mod
+++ go.mod
@@ -10,5 +10,7 @@ require (
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/samber/lo v1.38.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
 )
---
+++ internal/flycd/util/util_tab_table/tab_table.go
@@ -0,0 +1,51 @@
+package util_tab_table
+
+import (
+	"fmt"
+	"github.com/samber/lo"
+	"strings"
+)
+
+type TabTable struct {
+	Headers []string
+	Rows    [][]string
+}
+
+func ParseTable(table string) (TabTable, error) {
+
+	// Sanitize input into non-empty lines
+	lines := strings.Split(strings.TrimSpace(table), "\n")
+	lines = lo.Map(lines, func(item string, _ int) string {
+		return strings.TrimSpace(item)
+	})
+	lines = lo.Filter(lines, func(item string, _ int) bool {
+		return item != ""
+	})
+
+	if len(lines) == 0 {
+		return TabTable{}, fmt.Errorf("table has no lines, not even headers")
+	}
+
+	parseColumns := func(line string) []string {
+		items := strings.Split(line, "\t")
+		items = lo.Map(items, func(item string, _ int) string {
+			return strings.TrimSpace(item)
+		})
+		items = lo.Filter(items, func(item string, _ int) bool {
+			return item != ""
+		})
+		return items
+	}
+
+	result := TabTable{
+		Headers: parseColumns(lines[0]),
+	}
+
+	lines = lines[1:]
+	result.Rows = make([][]string, len(lines))
+	for i, line := range lines {
+		result.Rows[i] = parseColumns(line)
+	}
+
+	return result, nil
+}
