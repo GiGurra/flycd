e885b9c90732632679780de0103e739d5ec02c8b
Johan Kjölhede <gigurra@gmail.com>
2023-07-12 13:39:02 +0200

    wip

--- internal/flycd/flycd.go
+++ internal/flycd/flycd.go
@@ -151,7 +151,14 @@ func DeployAll(
 
 			case model.SourceTypeLocal:
 				// Use the local folder directly
-				innerResult, err := DeployAll(ctx, filepath.Join(project.Path, project.ProjectConfig.Source.Path), deployCfg)
+				absPath := func() string {
+					if filepath.IsAbs(project.ProjectConfig.Source.Path) {
+						return project.ProjectConfig.Source.Path
+					} else {
+						return filepath.Join(project.Path, project.ProjectConfig.Source.Path)
+					}
+				}
+				innerResult, err := DeployAll(ctx, absPath, deployCfg)
 				if err != nil {
 					return result, fmt.Errorf("deploying project %s: %w", project.ProjectConfig.Project, err)
 				}
