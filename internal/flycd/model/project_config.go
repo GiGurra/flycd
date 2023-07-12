c71246fa5c51e2e5ce81f99dc6e50dbd54ae094b
Johan Kjölhede <gigurra@gmail.com>
2023-07-12 11:47:10 +0200

    wip

--- internal/flycd/model/project_config.go
+++ internal/flycd/model/project_config.go
@@ -1,8 +1,13 @@
 package model
 
+import (
+	"fmt"
+	"regexp"
+)
+
 type ProjectConfig struct {
 	// Name Required. Name of the project
-	Name string `yaml:"name" toml:"name"`
+	Project string `yaml:"project" toml:"project"`
 	// Source Required. Where the app configs of the project are located
 	Source Source `yaml:"source" toml:"source"`
 	// Org Optional. Default org to be used by all apps in the project
@@ -11,4 +16,25 @@ type ProjectConfig struct {
 	PrimaryRegion string `yaml:"primary_region" toml:"primary_region"`
 	// Env Optional. Default env vars to be used by all apps in the project
 	Env map[string]string `yaml:"env" toml:"env"`
+}
+
+func (cfg *ProjectConfig) Validate() error {
+	if cfg.Project == "" {
+		return fmt.Errorf("project name is required")
+	}
+
+	// only permit apps that are valid dns names
+	const subdomainPrefixRegExp = `^[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?$`
+
+	subdomainPrefixChecker := regexp.MustCompile(subdomainPrefixRegExp)
+	if !subdomainPrefixChecker.MatchString(cfg.Project) {
+		return fmt.Errorf("project name '%s' is not a valid subdomain prefix", cfg.Project)
+	}
+
+	err := cfg.Source.Validate()
+	if err != nil {
+		return fmt.Errorf("project source is invalid: %w", err)
+	}
+
+	return nil
 }
