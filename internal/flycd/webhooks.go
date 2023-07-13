bcaeeebfa666ccbca6bfdeb44821e520de5606a8
Johan Kjölhede <gigurra@gmail.com>
2023-07-13 14:16:28 +0200

    wip

--- internal/flycd/webhooks.go
+++ internal/flycd/webhooks.go
@@ -43,6 +43,34 @@ func HandleGithubWebhook(payload github.PushWebhookPayload, path string) error {
 				}
 				return nil
 			},
+			ValidProjectCb: func(node model.ProjectNode) error {
+				// unfortunately we have to deploy everything here :S. This is because we don't know how far down
+				// the tree the change might affect our apps. So we have to deploy everything to be sure.
+				// It would be better to just use app repo webhooks instead, or at least group apps into small projects
+				localKey := strings.ToLower(node.ProjectConfig.Source.Repo)
+				remoteKeys := []string{
+					strings.ToLower(payload.Repository.Url),
+					strings.ToLower(payload.Repository.CloneUrl),
+					strings.ToLower(payload.Repository.HtmlUrl),
+					strings.ToLower(payload.Repository.GitUrl),
+					strings.ToLower(payload.Repository.SvnUrl),
+					strings.ToLower(payload.Repository.SshUrl),
+				}
+
+				if lo.Contains(remoteKeys, localKey) {
+					fmt.Printf("Found project %s matching webhook url %s. Deploying all apps in the project...\n", node.ProjectConfig.Project, payload.Repository.Url)
+
+					deployCfg := model.
+						NewDeployConfig().
+						WithRetries(1).
+						WithForce(false)
+					_, err := DeployAll(ctx, node.Path, deployCfg)
+					if err != nil {
+						fmt.Printf("Error deploying project %s: %v\n", node.ProjectConfig.Project, err)
+					}
+				}
+				return nil
+			},
 		})
 		if err != nil {
 			fmt.Printf("error traversing app tree: %v", err)
