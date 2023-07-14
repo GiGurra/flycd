94f4f83ffc473ead40f19490ce94e33f330c5de3
Johan Kjölhede <gigurra@gmail.com>
2023-07-14 17:04:00 +0200

    wip

--- internal/flycd/deployment_service_test.go
+++ internal/flycd/deployment_service_test.go
@@ -9,7 +9,7 @@ import (
 	"testing"
 )
 
-func TestDeployFromFolder(t *testing.T) {
+func TestDeployFromFolder_newApp(t *testing.T) {
 	ctx := context.Background()
 	flyClient := mocks.NewMockFlyClient(t)
 	deployService := NewDeployService(flyClient)
@@ -29,6 +29,39 @@ func TestDeployFromFolder(t *testing.T) {
 		EXPECT().
 		CreateNewApp(mock.Anything, mock.Anything, mock.Anything, mock.Anything).
 		Return(nil)
+
+	flyClient.
+		EXPECT().
+		DeployExistingApp(mock.Anything, mock.Anything, mock.Anything, mock.Anything).
+		Return(nil)
+
+	_, err := deployService.DeployAppFromFolder(ctx, "../../test/test-projects/merge-cfg/regular/app1", deployCfg)
+	if err != nil {
+		t.Fatalf("DeployAppFromFolder failed: %v", err)
+	}
+
+}
+
+func TestDeployFromFolder_existingApp(t *testing.T) {
+	ctx := context.Background()
+	flyClient := mocks.NewMockFlyClient(t)
+	deployService := NewDeployService(flyClient)
+	deployCfg := model.
+		NewDefaultDeployConfig().
+		WithAbortOnFirstError(true).
+		WithRetries(0)
+
+	fmt.Printf("flyClient: %+v\n", flyClient)
+
+	flyClient.
+		EXPECT().
+		ExistsApp(mock.Anything, mock.Anything).
+		Return(true, nil)
+
+	flyClient.
+		EXPECT().
+		GetDeployedAppConfig(mock.Anything, mock.Anything).
+		Return(model.AppConfig{}, nil)
 
 	flyClient.
 		EXPECT().
