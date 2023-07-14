5cdb420e7a9324a9dbdb36c4cbe7ad228ed12206
Johan Kjölhede <gigurra@gmail.com>
2023-07-14 23:24:44 +0200

    wip

--- internal/flycd/deployment_service_test.go
+++ internal/flycd/deployment_service_test.go
@@ -108,59 +108,90 @@ func TestDeployFromFolder_appMergingConfig(t *testing.T) {
 
 }
 
-func TestDeployFromFolder_nginxWithVolumes(t *testing.T) {
-	ctx := context.Background()
-	flyClient := mocks.NewMockFlyClient(t)
-	deployService := NewDeployService(flyClient)
-	deployCfg := model.
-		NewDefaultDeployConfig().
-		WithAbortOnFirstError(true).
-		WithRetries(0)
+func TestDeployFromFolder_withVolumes(t *testing.T) {
 
-	fmt.Printf("flyClient: %+v\n", flyClient)
+	for _, test := range []struct {
+		name              string
+		deployCfg         model.DeployConfig
+		deployedAppScale  int
+		numResizedVolumes int
+		numCreatedVolumes int
+	}{
+		{
+			name: "create volumes - current app scale decides",
+			deployCfg: model.
+				NewDefaultDeployConfig().
+				WithAbortOnFirstError(true).
+				WithRetries(0),
+			deployedAppScale:  4,
+			numResizedVolumes: 0,
+			numCreatedVolumes: 4,
+		},
+		{
+			name: "create volumes - minimum services count decides",
+			deployCfg: model.
+				NewDefaultDeployConfig().
+				WithAbortOnFirstError(true).
+				WithRetries(0),
+			deployedAppScale:  0,
+			numResizedVolumes: 0,
+			numCreatedVolumes: 3,
+		},
+	} {
 
-	flyClient.
-		EXPECT().
-		ExistsApp(mock.Anything, mock.Anything).
-		Return(true, nil)
+		t.Run(test.name, func(t *testing.T) {
 
-	flyClient.
-		EXPECT().
-		GetDeployedAppConfig(mock.Anything, mock.Anything).
-		Return(model.AppConfig{}, nil)
+			ctx := context.Background()
+			flyClient := mocks.NewMockFlyClient(t)
+			deployService := NewDeployService(flyClient)
 
-	flyClient.
-		EXPECT().
-		GetAppVolumes(mock.Anything, mock.Anything).
-		Return([]model.VolumeState{}, nil)
+			fmt.Printf("flyClient: %+v\n", flyClient)
 
-	flyClient.
-		EXPECT().
-		GetAppScale(mock.Anything, mock.Anything).
-		Return([]model.ScaleState{
-			{
-				Process: "app",
-				Count:   2,
-			},
-		}, nil)
+			flyClient.
+				EXPECT().
+				ExistsApp(mock.Anything, mock.Anything).
+				Return(true, nil)
 
-	flyClient.
-		EXPECT().
-		CreateVolume(mock.Anything, "nginx-with-volumes-test", model.VolumeConfig{
-			Name:   "data",
-			SizeGb: 10,
-			Region: "arn",
-		}).
-		Return(model.VolumeState{}, nil)
+			flyClient.
+				EXPECT().
+				GetDeployedAppConfig(mock.Anything, mock.Anything).
+				Return(model.AppConfig{}, nil)
 
-	flyClient.
-		EXPECT().
-		DeployExistingApp(mock.Anything, mock.Anything, mock.Anything, mock.Anything).
-		Return(nil)
+			flyClient.
+				EXPECT().
+				GetAppVolumes(mock.Anything, mock.Anything).
+				Return([]model.VolumeState{}, nil)
 
-	_, err := deployService.DeployAppFromFolder(ctx, "../../test/test-projects/nginx-with-volumes/app", deployCfg)
-	if err != nil {
-		t.Fatalf("DeployAppFromFolder failed: %v", err)
+			flyClient.
+				EXPECT().
+				GetAppScale(mock.Anything, mock.Anything).
+				Return([]model.ScaleState{
+					{
+						Process: "app",
+						Count:   test.deployedAppScale,
+					},
+				}, nil)
+
+			flyClient.
+				EXPECT().
+				CreateVolume(mock.Anything, "nginx-with-volumes-test", model.VolumeConfig{
+					Name:   "data",
+					SizeGb: 10,
+					Region: "arn",
+				}).
+				Return(model.VolumeState{}, nil).
+				Times(test.numCreatedVolumes)
+
+			flyClient.
+				EXPECT().
+				DeployExistingApp(mock.Anything, mock.Anything, mock.Anything, mock.Anything).
+				Return(nil)
+
+			_, err := deployService.DeployAppFromFolder(ctx, "../../test/test-projects/nginx-with-volumes/app", test.deployCfg)
+			if err != nil {
+				t.Fatalf("DeployAppFromFolder failed: %v", err)
+			}
+		})
 	}
 
 }
