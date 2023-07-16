8cc415f5e2144ea3812cdfcd283f80bf023b3f3e
Johan Kjölhede <gigurra@gmail.com>
2023-07-16 11:21:09 +0200

    wip

--- scripts/bump_version.bash
+++ scripts/bump_version.bash
@@ -65,10 +65,10 @@ if [[ $(git ls-remote --tags origin "NEXT_TAG") ]]; then
 fi
 
 # Update the tag in main.go
-sed -i "main.go" "s/$CUR_TAG/$NEXT_TAG/g"
+sed -i -e "s/$CUR_TAG/$NEXT_TAG/g" "main.go"
 
 # Update the tag in the README
-sed -i "README.md" "s/$PREV_TAG/$CUR_TAG/g"
+sed -i -e "s/$PREV_TAG/$CUR_TAG/g" "README.md"
 
 # add the changes to git
 git add main.go README.md
