456bf3848a8d029d1b815407e2aecbaeb5e1c539
Johan Kjölhede <gigurra@gmail.com>
2023-07-25 00:22:08 +0200

    fix shutdown signal handler for monitoring mode, to interrupt less webhook jobs

--- cmd/monitor/monitor.go
+++ cmd/monitor/monitor.go
@@ -52,23 +52,25 @@ func defaultWhPort() int {
 }
 
 func handleShutdown(shutdownHandler func()) {
-	sig := make(chan os.Signal, 1)
-	signal.Notify(
-		sig,
-		// Possible fly.io shutdown signals
-		syscall.SIGINT,
-		syscall.SIGTERM,
-		syscall.SIGQUIT,
-		syscall.SIGUSR1,
-		syscall.SIGUSR2,
-		syscall.SIGKILL,
-		syscall.SIGSTOP,
-	)
-	select {
-	case s := <-sig:
-		fmt.Printf("Received signal: %s\n", s.String())
-		shutdownHandler()
-	}
+	go func() {
+		sig := make(chan os.Signal, 1)
+		signal.Notify(
+			sig,
+			// Possible fly.io shutdown signals
+			syscall.SIGINT,
+			syscall.SIGTERM,
+			syscall.SIGQUIT,
+			syscall.SIGUSR1,
+			syscall.SIGUSR2,
+			syscall.SIGKILL,
+			syscall.SIGSTOP,
+		)
+		select {
+		case s := <-sig:
+			fmt.Printf("Received signal: %s\n", s.String())
+			shutdownHandler()
+		}
+	}()
 }
 
 func Cmd(
